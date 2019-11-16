d4505e0e4f42f48a0aa212a4b5bfde57340cb420
Your Name <535378755@qq.com>
2019-11-16 15:50:27 +0800

    koa-cors插件

--- server/index.ts
+++ server/index.ts
@@ -4,8 +4,8 @@ import * as bodyParser from 'koa-bodyparser';
 import * as Static from 'koa-static';
 import * as koaSession from 'koa-session';
 import * as path from 'path';
-// import _ from 'koa-mysql-session';
-import * as sess from 'koa-session-minimal';
+import * as Cors from 'koa2-cors';
+
 
 import { createConnection } from 'typeorm';
 import User from './entities/user';
@@ -41,25 +41,28 @@ createConnection({
 }).then(connection => {
   const staticPath = './static'
   const app = new Koa();
-  const session = koaSession(session_config, app);
   // app.keys = session_signed_key;
-  app.use(session);
+  app.use(koaSession(session_config, app));
   app.use(Static(
     path.join(__dirname, staticPath)
-  ))
+  ));
+  app.use(Cors({
+    origin: (ctx) => {
+      if (ctx.url === '/test') {
+        return false;
+      }
+      return 'http://localhost:4200';
+    },
+    allowMethods: ['OPTIONS', 'GET', 'PUT', 'POST', 'DELETE'],
+    allowHeaders: ['x-requested-with', 'accept', 'origin', 'content-type'],
+    credentials: true,
+  }));
   app.use(async (ctx, next) => {
-    ctx.set('Access-Control-Allow-Origin', 'http://localhost:4200');
-    // 设置所允许的HTTP请求方法
-    ctx.set('Access-Control-Allow-Methods', 'OPTIONS, GET, PUT, POST, DELETE');
-    ctx.set('Access-Control-Allow-Credentials', 'true');
-    // 字段是必需的。它也是一个逗号分隔的字符串，表明服务器支持的所有头信息字段.
-    ctx.set('Access-Control-Allow-Headers', 'x-requested-with, x-auth-token, accept, origin, content-type')
     if ( ctx.method === 'OPTIONS' || ctx.path === '/rest/v1.0/login') {
       await next();
       return;
     }
     const key = ctx.cookies.get('SESSIOND_ID');
-    console.log(key);
     if (!key) {
       ctx.status = 401;
       ctx.body = 'unauth';
@@ -67,7 +70,6 @@ createConnection({
       await next();
     }
     // await next();
-    
   });
   app.use(bodyParser());
   const router = new Router();
