035cf58f8ee5c834c15fe0c91ac7102a6f875058
lvzero535 <535378755@qq.com>
2022-01-06 23:25:02 +0800

    feat: add hushy

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,21 @@
+
 module.exports = {
-  extends: ['@commitlint/config-conventional']
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'fix-td-rule': [2, 'always'],
+  },
+  plugins: [
+    {
+      rules: {
+        'fix-td-rule': ({subject, type}) => {
+          console.log(subject, type)
+          const hasTD = type === 'fix' && !/\[\d\]/.test(subject) 
+          return [
+            !hasTD,
+            `Your subject should contain ${type}, ${hasTD} message`,
+          ];
+        },
+      },
+    },
+  ]
 }
